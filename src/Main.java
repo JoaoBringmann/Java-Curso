73106a58a91893111427830e3c8dd51f8e796bed
JoaoBringmann <149213160+JoaoBringmann@users.noreply.github.com>
2025-03-15 21:26:57 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+        //This is java Program
+
+        /*
+        sout => Abreviação so system.out
+        */
+    public static void main(String[] args) {
+
+        System.out.println("Eu gosto de banana");
+        System.out.print("Eu sou muito bom");
+
+    }
+}
